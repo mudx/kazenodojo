db1200d0cfa0d307afe5e3a18a99ce99c34cdf00
mudx <mudx@users.noreply.github.com>
2022-03-21 23:41:12 -0300

    Update dojoconf.py

--- dojoconf.py
+++ dojoconf.py
@@ -2,7 +2,7 @@ import os
 
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 587
-MAIL_USERNAME = "elopez.perto@gmail.com"
-MAIL_PASSWORD = "flensburgeR1725#"
+MAIL_USERNAME = "
+MAIL_PASSWORD = ""
 MAIL_USE_TLS = 1
 MAIL_USE_SSL = 0
